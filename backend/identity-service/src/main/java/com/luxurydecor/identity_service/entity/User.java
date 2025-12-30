9788ad9f0d0a806ff93276fe14c8be0060c372a7
NgoTan <ngovantannvt04@gmail.com>
2025-12-30 16:54:08 +0700

    chore: initialize project structure and identity service

---
+++ backend/identity-service/src/main/java/com/luxurydecor/identity_service/entity/User.java
@@ -0,0 +1,28 @@
+package com.luxurydecor.identity_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long account_id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+}
